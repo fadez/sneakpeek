4e5744936c3e446b79761bfdaa49f5153dc47a70
Alex Fadez <11855162+fadez@users.noreply.github.com>
2025-05-31 22:06:37 +0300

    chore: add Vue

---
+++ resources/js/App.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+    <div class="max-w-2xl mx-auto p-4">
+        <h1 class="text-2xl font-bold text-center">Welcome to SneakPeek!</h1>
+    </div>
+</template>
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,22 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import path from 'path';
 import tailwindcss from '@tailwindcss/vite';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'resources/js'),
+            'vue': path.resolve(__dirname, 'node_modules/vue/dist/vue.esm-bundler.js'),
+        },
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
         tailwindcss(),
+        vue(),
     ],
 });
