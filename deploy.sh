33f6cc17d5da9e5db87ee96602924a3c7f349362
Alex Fadez <11855162+fadez@users.noreply.github.com>
2025-08-11 01:46:57 +0300

    chore: add deploy script

---
+++ deploy.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Put app into maintenance mode
+php artisan down
+
+# Pull the latest updates
+git pull --ff-only
+
+# Install composer dependencies
+composer install --optimize-autoloader --prefer-dist --no-dev --no-interaction
+
+# Build assets
+npm ci
+npm run build
+
+# Clear all cached data
+php artisan view:clear
+php artisan cache:clear
+
+# Cache configuration
+php artisan config:cache
+
+# Run Laravel migrations
+php artisan migrate --force
+
+# Reload php8.2-fpm if running
+if systemctl is-active --quiet php8.2-fpm; then
+    sudo systemctl reload php8.2-fpm
+fi
+
+# Reload php8.3-fpm if running
+if systemctl is-active --quiet php8.3-fpm; then
+    sudo systemctl reload php8.3-fpm
+fi
+
+# Reload php8.4-fpm if running
+if systemctl is-active --quiet php8.4-fpm; then
+    sudo systemctl reload php8.4-fpm
+fi
+
+# Reload nginx if running
+if systemctl is-active --quiet nginx; then
+    sudo systemctl reload nginx
+fi
+
+# Bring app out of maintenance mode
+php artisan up
