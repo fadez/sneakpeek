ba1e9d6887f18653b83d1e6a24f8ae801fe77d00
Alex Fadez <11855162+fadez@users.noreply.github.com>
2025-08-12 15:06:25 +0300

    feat: add secret route about me

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/who-is-alex-fadez', function () {
+    abort(418);
+});
+
 Route::fallback(function () {
     return view('app');
 })->name('home');
