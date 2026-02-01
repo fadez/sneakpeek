5f425f230a6d200c2290d8077ea2a8dbdb0a1386
Alex Fadez <11855162+fadez@users.noreply.github.com>
2026-02-01 20:35:10 +0200

    feat: add source code link to footer

--- resources/js/components/TheFooter.vue
+++ resources/js/components/TheFooter.vue
@@ -5,14 +5,16 @@ import BaseLink from '@/components/BaseLink.vue';
 <template>
     <footer class="container max-w-3xl mx-auto px-4 my-4">
         <div class="text-center text-zinc-700 dark:text-zinc-400">
-            © {{ new Date().getFullYear() }}
             Built with ❤️ in 🇺🇦 by
-            <BaseLink
-                href="https://github.com/fadez"
-                target="_blank"
-            >
-                @fadez
-            </BaseLink>
+            <BaseLink href="https://github.com/fadez" target="_blank"">@fadez</BaseLink>
+        </div>
+        <div class="text-center text-zinc-700 dark:text-zinc-400 mt-4">
+            View source code on
+            <BaseLink href="https://github.com/fadez/sneakpeek" target="_blank">GitHub</BaseLink>
+        </div>
+
+        <div class="text-center text-xs text-zinc-600 dark:text-zinc-300 mt-4">
+            © {{ new Date().getFullYear() }} SneakPeek
         </div>
     </footer>
 </template>
