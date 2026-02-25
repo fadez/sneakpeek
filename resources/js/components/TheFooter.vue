dbbff44e3ab5557d187bf6ac7b1a62480c63ef4f
Alex Fadez <11855162+fadez@users.noreply.github.com>
2026-02-25 13:39:07 +0200

    chore: add copyright to footer

--- resources/js/components/TheFooter.vue
+++ resources/js/components/TheFooter.vue
@@ -1,22 +1,29 @@
 <script setup>
 import BaseLink from '@/components/BaseLink.vue';
 
+const appName = import.meta.env.VITE_APP_NAME;
 const commitHash = __COMMIT_HASH__;
 </script>
 
 <template>
     <footer data-test="footer" class="container mx-auto my-4 max-w-4xl px-4">
-        <div class="text-center text-zinc-700 dark:text-zinc-400">
-            Built with ❤️ in 🇺🇦 by
-            <BaseLink href="https://github.com/fadez" target="_blank">@fadez</BaseLink>
+        <div class="flex justify-between text-zinc-700 dark:text-zinc-400">
+            <div>© {{ new Date().getFullYear() }} {{ appName }}. All rights reserved.</div>
+
+            <div>
+                View source code on
+                <BaseLink href="https://github.com/fadez/sneakpeek" target="_blank">GitHub</BaseLink>
+            </div>
         </div>
-        <div class="mt-4 text-center text-zinc-700 dark:text-zinc-400">
-            View source code on
-            <BaseLink href="https://github.com/fadez/sneakpeek" target="_blank">GitHub</BaseLink>
-        </div>
-        <div v-if="commitHash" class="mt-4 text-center text-xs text-zinc-400 dark:text-zinc-500">
-            Current version:
-            <BaseLink :href="`https://github.com/fadez/sneakpeek/commit/${commitHash}`" target="_blank">{{ commitHash }}</BaseLink>
+        <div class="flex justify-between text-zinc-400 dark:text-zinc-500">
+            <div>
+                Built with ❤️ in 🇺🇦 by
+                <BaseLink href="https://github.com/fadez" target="_blank">@fadez</BaseLink>
+            </div>
+            <div v-if="commitHash">
+                Current version:
+                <BaseLink :href="`https://github.com/fadez/sneakpeek/commit/${commitHash}`" target="_blank">{{ commitHash }}</BaseLink>
+            </div>
         </div>
     </footer>
 </template>
