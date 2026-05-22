1868b99fa9bcd82f6aeb3d6eca2145a6bbbfca7e
Alex Fadez <11855162+fadez@users.noreply.github.com>
2026-05-22 16:21:18 +0300

    refactor: replace axios with native fetch

--- resources/js/api.js
+++ resources/js/api.js
@@ -1,37 +1,22 @@
-import axios from '@/axios';
+import { http } from '@/http';
 
-export const getStatistics = () => {
-    return axios.get('/api/statistics').then((response) => response.data);
-};
+export const getStatistics = () => http.get('/api/statistics');
 
-export const getFeatures = () => {
-    return axios.get('/api/features').then((response) => response.data);
-};
+export const getFeatures = () => http.get('/api/features');
 
-export const deactivateFeature = (feature) => {
-    return axios.post(`/api/features/${feature}/deactivate`);
-};
+export const deactivateFeature = (feature) => http.post(`/api/features/${feature}/deactivate`);
 
-export const storeSecret = (data) => {
-    return axios.post('/api/secrets', data).then((response) => response.data.secret);
-};
+export const storeSecret = (data) => http.post('/api/secrets', data).then((response) => response.secret);
 
-export const getSecret = (id, accessToken = undefined) => {
-    return axios
+export const getSecret = (id, accessToken = undefined) =>
+    http
         .get(`/api/secrets/${id}`, {
-            // Access token is passed via header instead of query string to prevent
-            // it from appearing in server logs, browser history, and referrer headers
             headers: {
                 ...(accessToken && { 'X-Access-Token': accessToken }),
             },
         })
-        .then((response) => response.data.secret);
-};
+        .then((response) => response.secret);
 
-export const revealSecret = (id, data) => {
-    return axios.post(`/api/secrets/${id}/reveal`, data).then((response) => response.data.content);
-};
+export const revealSecret = (id, data) => http.post(`/api/secrets/${id}/reveal`, data).then((response) => response.content);
 
-export const burnSecret = (id, data) => {
-    return axios.delete(`/api/secrets/${id}`, { data });
-};
+export const burnSecret = (id, data) => http.delete(`/api/secrets/${id}`, { body: data });
