44af289c7c8fec1de5d0ddafe6cff27f4cc77a63
Alex Fadez <11855162+fadez@users.noreply.github.com>
2026-08-05 11:54:41 +0300

    fix: correct font weight on error pages

--- resources/views/layouts/error.blade.php
+++ resources/views/layouts/error.blade.php
@@ -6,9 +6,13 @@
     <div class="http-error-container">
         <div class="http-error-card">
             <h1 class="http-error-code">@yield('code')</h1>
+
             @hasSection('imageUrl')
-                <img src="@yield('imageUrl')" class="http-error-image" alt="@yield('code')">
+                <a href="{{ url('/') }}">
+                    <img src="@yield('imageUrl')" class="http-error-image" alt="@yield('code')">
+                </a>
             @endif
+
             <div class="http-error-message">@yield('message')</div>
         </div>
     </div>
